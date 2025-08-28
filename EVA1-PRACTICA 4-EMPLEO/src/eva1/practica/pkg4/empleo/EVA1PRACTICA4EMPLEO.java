1d5b3cf5961ddc94fa5ff4bd6f6b81b7b558f455
Jahaziel1305 <L25550366@chihuahua2.tecnm.mx>
2025-08-27 22:01:09 -0600

    Add files via upload

---
+++ EVA1-PRACTICA 4-EMPLEO/src/eva1/practica/pkg4/empleo/EVA1PRACTICA4EMPLEO.java
@@ -0,0 +1,20 @@
+package eva1.practica.pkg4.empleo;
+
+public class EVA1PRACTICA4EMPLEO {
+
+    public static void main(String[] args) {
+
+        System.out.print("Empleo: ");
+        System.out.println("Rectificador de motores");
+        System.out.print("Salario (semanal): ");
+        System.out.println("2200 Pesos mexicanos");
+        System.out.print("Horas minimas por semana: ");
+        System.out.println(48);
+        System.out.print("Requiere ingles?: ");
+        System.out.println("FALSE");
+    
+        
+        
+    }
+    
+}
