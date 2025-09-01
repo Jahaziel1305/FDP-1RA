a5ccec7208a5aa8a2049e215165b14c8c9217296
Jahaziel1305 <L25550366@chihuahua2.tecnm.mx>
2025-09-01 15:58:53 -0600

    Add files via upload

---
+++ EVA1-PRACTICA 5-VARIABLES/src/eva1/practica/pkg5/variables/EVA1PRACTICA5VARIABLES.java
@@ -0,0 +1,21 @@
+package eva1.practica.pkg5.variables;
+
+public class EVA1PRACTICA5VARIABLES {
+
+    public static void main(String[] args) {
+        //DECLARAR VARIABLES
+        //TIPO DE DATO Y LUEGO EL NOMBRE DEL IDENTIFICADOR 
+        String nombre;
+        int edad; 
+        double salario;
+        //INICIALIZACION
+        //IDENTIFICADOR=VALOR;
+        //=--> OPERADOR DE ASIGNACION
+        nombre ="Jahaziel Omar Ortiz Quintana";
+        edad = 18;
+        salario = 364.8;
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.print(salario); 
+    }
+}
