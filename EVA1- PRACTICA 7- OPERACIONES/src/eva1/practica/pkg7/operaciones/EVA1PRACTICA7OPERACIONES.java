778921d6dd00b1015c3b61c8025a981c1e8f96c1
Jahaziel1305 <L25550366@chihuahua2.tecnm.mx>
2025-09-01 16:44:33 -0600

    Add files via upload

---
+++ EVA1- PRACTICA 7- OPERACIONES/src/eva1/practica/pkg7/operaciones/EVA1PRACTICA7OPERACIONES.java
@@ -0,0 +1,14 @@
+package eva1.practica.pkg7.operaciones;
+
+public class EVA1PRACTICA7OPERACIONES {
+
+public static void main(String[] args) {
+
+double base, altura, area;
+base = 5;
+altura = 10;
+area = (base*altura)/2;
+System.out.print("El area del triangulo es: ");
+System.out.println(area);
+}
+ }
---
+++ EVA1-PRACTICA 6-OPERACIONES/src/eva1/practica/pkg6/operaciones/EVA1PRACTICA6OPERACIONES.java
@@ -0,0 +1,14 @@
+package eva1.practica.pkg6.operaciones;
+
+public class EVA1PRACTICA6OPERACIONES {
+
+public static void main(String[] args) {
+        
+double area, p, radio;
+p = 3.1416;
+radio = 3;
+area = p*radio*radio;
+System.out.print("El area del circulo es: ");
+System.out.println(area);
+}
+ }
