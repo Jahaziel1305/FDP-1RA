2c92093b604d6490f8e030e380608d95da434839
Jahaziel1305 <L25550366@chihuahua2.tecnm.mx>
2025-09-01 22:16:08 -0600

    Add files via upload

---
+++ EVA1-PRACTICA8-TEMPERATURAS/src/main/java/com/mycompany/eva1/practica8/temperaturas/EVA1PRACTICA8TEMPERATURAS.java
@@ -0,0 +1,12 @@
+package com.mycompany.eva1.practica8.temperaturas;
+
+public class EVA1PRACTICA8TEMPERATURAS {
+
+public static void main(String[] args) {
+double farenheit, celsius;
+farenheit = 87;
+celsius = 5 * (farenheit - 32) / 9;
+System.out.print("Los grados Celsius son: " + celsius); //SE COLOCA EL TEXTO ENTRE COMILLAS + LA ASIGNACION
+               
+    }
+}
