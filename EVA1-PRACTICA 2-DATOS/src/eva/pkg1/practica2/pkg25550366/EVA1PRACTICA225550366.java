68c693e8ca699c5c4d6c1de84bf5b4b9d99b7dcf
Jahaziel <l25550366@chihuahua2.tecnm.mx>
2025-09-22 13:40:35 -0600

    #

--- EVA1-PRACTICA 2-DATOS/src/eva/pkg1/practica2/pkg25550366/EVA1PRACTICA225550366.java
+++ EVA1-PRACTICA 2-DATOS/src/eva/pkg1/practica2/pkg25550366/EVA1PRACTICA225550366.java
@@ -1,32 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package eva.pkg1.practica2.pkg25550366;
 
-/**
- *
- * @author alond
- */
 public class EVA1PRACTICA225550366 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        //;SIGNIFICA FIN DE LINEA
-        //" --> CADENA DE TEXTO .DATO
+        // ;SIGNIFICA FIN DE LINEA
+        // " --> CADENA DE TEXTO .DATO
         System.out.println("JAHAZIEL ORTIZ");
-        //NUMEROS ENTEROS
+        // NUMEROS ENTEROS
         System.out.println(2007);
-        //NUMEROS DECIMALES
+        // NUMEROS DECIMALES
         System.out.println(1003.5);
-        //VALORES LOGICOS (VERDADERO O FALSO)
-        //TRUE OR FALSE
+        // VALORES LOGICOS (VERDADERO O FALSO)
+        // TRUE OR FALSE
         System.out.println(true);
-        //CARACTERES INDIVIDUALES
-        System.out.println('c');//CARACTER
-        System.out.println("c");//CADENA DE TEXTO
-                }
-    
+        // CARACTERES INDIVIDUALES
+        System.out.println('c'); // CARACTER
+        System.out.println("c"); // CADENA DE TEXTO
+    }
 }
