8002bc087c46f2f5cb417af65bd26df597a617c1
Jahaziel <l25550366@chihuahua2.tecnm.mx>
2025-09-22 13:42:16 -0600

    #

--- EVA1-PRACTICA 3-DATOS/src/eva1/practica/pkg3/datos/EVA1PRACTICA3DATOS.java
+++ EVA1-PRACTICA 3-DATOS/src/eva1/practica/pkg3/datos/EVA1PRACTICA3DATOS.java
@@ -3,12 +3,12 @@ package eva1.practica.pkg3.datos;
 public class EVA1PRACTICA3DATOS {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+
         System.out.print("NOMBRE DEL ALUMNO: ");
         System.out.println("JAHAZIEL OMAR ORTIZ QUINTANA");
-        System.out.print("NUMERO DE CONTROL: ");
+        System.out.print("NÚMERO DE CONTROL: ");
         System.out.println(25550366);
         System.out.print("EDAD: ");
         System.out.println(18);
     }
-    }
+}
